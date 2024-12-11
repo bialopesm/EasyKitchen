674824eb1376360e9ba7769109560fdaec492b16
Beatriz Machado <bialmachado@gmail.com>
2024-12-11 14:42:14 -0300

    Ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,6 +4,7 @@ class IngredientsController < ApplicationController
 
   def index
     @ingredients = Ingredient.ordered
+    @ingredient = Ingredient.new
   end
 
   def new
@@ -21,7 +22,11 @@ class IngredientsController < ApplicationController
     if @ingredient.save
       respond_to do |format|
         format.html { redirect_to ingredients_path, notice: "ingredient was successfully created." }
-        format.turbo_stream
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(:ingredients, partial: "ingredients/ingredient",
+            locals: { ingredient: @ingredient })
+        end
+        format.html { redirect_to ingredient_path(@ingredient) }
       end
     else
       # @ingredients = Ingredient.all
