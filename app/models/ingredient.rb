1096aed5eec13730723fc0fa2cbfdc1037d56a82
Nominoe Girard <nominoe.girard@gmail.com>
2024-12-12 16:28:39 -0300

    ingredient page designed

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,15 @@
 class Ingredient < ApplicationRecord
 
   validates :name, presence: true
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than: 0 }, if: :quantity_present?
 
   scope :ordered, -> { order(id: :desc) }
 
+  private
+
+  def quantity_present?
+    quantity.present?
+  end
 
 end
