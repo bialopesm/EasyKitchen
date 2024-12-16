2468dea0975138f527ace45a782a636243d39bbf
Nominoe Girard <nominoe.girard@gmail.com>
2024-12-16 17:47:08 -0300

    last page update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,18 +5,22 @@ class CommentsController < ApplicationController
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @comment = Comment.new(recipe: @recipe)
-    @comments = Comment.all
+    @comments = @recipe.comments
   end
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @comment = @recipe.comments.new(comment_params)
 
+    if params[:recipe] && params[:recipe][:rating].present?
+      @recipe.update(rating: params[:recipe][:rating])
+    end
 
     if @comment.save
-      redirect_to bookmarks_path, notice: 'It was created!'
+      redirect_to new_recipe_comment_path, notice: 'Comment added !'
     else
-      render :new, alert: 'Error. Could not create.'
+      flash.now[:alert] = 'Error. Could not create the comment.'
+      render :new
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :recipe
+
+  validates :content, presence: true
 end
